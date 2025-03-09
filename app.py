4a9eabcdd80261daff92f86a5433e882098d9907
Avigail cohen <avigailcohen17@gmail.com>
2025-03-09 20:32:22 +0200

    testing again

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def is_valid_url(url):
     """בודק אם הקלט הוא URL תקף"""
     url_regex = re.compile(
         r'^(https?:\/\/)?' 
-        r'(([A-Za-z0-9-]+\.)+[A-Za-z]{2,6}|'  # שם דומיין
+        r'(([A-Za-z0-9-]+\.)+[A-Za-z]{2,6}|'  
         r'localhost|'  # או localhost
         r'\d{1,3}(\.\d{1,3}){3})'  # או כתובת IP
         r'(:\d+)?(\/[^\s]*)?$'  # פורט אופציונלי ושאר הנתיב
